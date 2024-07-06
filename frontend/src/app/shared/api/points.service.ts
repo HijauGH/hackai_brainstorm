7e1a2646ad3c385c088ac0d8caecdb7c18a7ab16
AUKYJL <89800289+AUKYJL@users.noreply.github.com>
2024-07-06 20:07:19 +0500

    fix: fix points request

--- frontend/src/app/shared/api/points.service.ts
+++ frontend/src/app/shared/api/points.service.ts
@@ -1,13 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
-
+import { HttpHeaders } from '@angular/common/http';
+export const headers = new HttpHeaders({
+	'Content-Type': 'application/json',
+});
 @Injectable({ providedIn: 'root' })
 export class pointsService {
   constructor(private http: HttpClient) {}
 
   public getAllPoints() {
-    return this.http.get<any>(environment.apiUrl).subscribe(d => {
+    return this.http.post<any>(environment.apiUrl,{
+			"adSides": 0,
+			"gender": "male",
+			"ageFrom": 0,
+			"ageTo": 0,
+			"income": "0",
+			"type": 0
+	},{headers:headers}).subscribe(d => {
       console.log(d);
     });
   }
